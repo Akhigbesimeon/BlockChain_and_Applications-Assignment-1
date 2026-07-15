ef349f11b9731b620c9ce8d0dc381b7de8df5121
Akhigbesimeon <s.akhigbe@alustudent.com>
2026-07-15 17:18:32 +0200

    changed the REGISTRY_ADDRESS & TOKEN_ADDRESS

--- alu-logo-registry-dapp/src/context/wallet_context.tsx
+++ alu-logo-registry-dapp/src/context/wallet_context.tsx
@@ -3,8 +3,8 @@ import { ethers } from 'ethers';
 import RegistryABI from '../contracts/ALUAssetRegistry.json';
 import TokenABI from '../contracts/ALULogoToken.json';
 
-const REGISTRY_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-const TOKEN_ADDRESS = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+const REGISTRY_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+const TOKEN_ADDRESS = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
 
 interface WalletContextType {
   account: string | null;
